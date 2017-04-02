30f034b04fc516d0ee4e9a8994fea7ef640f33c4
Wasee Rehman <wasi_rehman@live.com>
2017-04-02 23:41:08 +0530

    Course & Batch Added

--- Omnimerit_Portal/Controllers/PortalController.cs
+++ Omnimerit_Portal/Controllers/PortalController.cs
@@ -13,5 +13,6 @@ namespace Omnimerit_Portal.Controllers
         {
             return View();
         }
+
     }
 }
--- Omnimerit_Portal/Global.asax.cs
+++ Omnimerit_Portal/Global.asax.cs
@@ -9,10 +9,24 @@ namespace Omnimerit_Portal
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            //AreaRegistration.RegisterAllAreas();
+            // RouteConfig.RegisterRoutes(RouteTable.Routes);
+            RegisterRoutes(RouteTable.Routes);
+
+        }
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            //routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default",                                              // Route name
+                "{controller}/{action}/{id}",                           // URL with parameters
+                new { controller = "Portal", action = "Index", id = "" }  // Parameter defaults
+            );
+
         }
     }
 }
