209c8b96c259e98e66e83bc48b5fcacb9b21a449
Wasee Rehman <wasi_rehman@live.com>
2017-05-10 11:18:25 +0530

    Data project Added

---
+++ Omnimerit.Data/BussinessLayer/Business.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Omnimerit.Data.BussinessLayer
+{
+    public class Business
+    {
+
+        //here we will have business method
+
+
+    }
+}
