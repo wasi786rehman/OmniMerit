0f1e3de66f5c36c41b86c57aa4db007cb48489ae
Wasee Rehman <wasi_rehman@live.com>
2017-05-20 12:46:48 +0530

    Database Added

--- Omnimerit.Data/BussinessLayer/Business.cs
+++ Omnimerit.Data/BussinessLayer/Business.cs
@@ -3,13 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Omnimerit.Data.Model.Database;
 
 namespace Omnimerit.Data.BussinessLayer
 {
     public class Business
     {
+        
+        public bool Login(login login)
+        {
+            try
+            {
+                double number = Convert.ToDouble(login.Id);
+                double password = Convert.ToDouble(login.Password);
+                using (OmnimeritEntities modelentity = new OmnimeritEntities())
+                {
+                    var v = modelentity.AirResults.Where(model => model.Mobile_No == number && model.Mobile_No == password).Count();
 
-        //here we will have business method
+                    if (v == 1)
+                        return true;
+                    else
+                        return false;
+                }
+            }
+            catch (Exception e) { return false; }
+        }
 
 
     }
--- Omnimerit_Portal/Controllers/PortalController.cs
+++ Omnimerit_Portal/Controllers/PortalController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Omnimerit.Data.Model.Database;
+using Omnimerit.Data.BussinessLayer;
 namespace Omnimerit_Portal.Controllers
 {
     public class PortalController : Controller
@@ -12,6 +13,30 @@ namespace Omnimerit_Portal.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+       
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(login login)
+        {
+           
+
+            var errors = ModelState.Values.SelectMany(v => v.Errors);
+
+            if (ModelState.IsValid)
+            {
+                Business business = new Business();
+                if (business.Login(login))
+                {
+                    TempData["number"] = login.Id;
+                    return RedirectToAction("../default/profile");
+                }
+                else
+                    return RedirectToAction("../Default/invalidcredential");
+            }
+
+            ViewBag.Message = "Invalid Login Details";
+            return RedirectToAction("../Default/invalidcredential");
         }
 
     }
